66624429a362c679ca89501a36e87904afc37bbc
big-mike2 <migbike25@gmail.com>
2025-10-26 22:26:28 -0400

    MADE FLASHCARDS EDITABLE

--- scripts/create_deck.js
+++ scripts/create_deck.js
@@ -8,6 +8,8 @@ var deck_viewer = document.getElementById("deck_viewer");
 
 var deck_is_viewed = false;
 
+var currently_editing = false;
+
 
 //class for the deck of flashcards, it should have a name, description, and deck
 
@@ -48,10 +50,8 @@ function add_card(){
 
   let card = [front, back];
   current_deck.push(card);
-
-  if(deck_is_viewed){
-    render_cards();
-  }
+//previously we had if the deck is viewed render cards, but i don't know if it was necessary.
+  render_cards();
 }
 
 //makes a deck based on the current values (must be filled out), returns false early if not
@@ -65,12 +65,23 @@ function makeDeck(){
   if(current_deck == []){
     return false;
   }
+  //saves all the edits made to the deck
+  console.log("got it");
+  let array_of_flashcards = deck_viewer.children;
+  for(var i = 0; i < current_deck.length; i++){
+    let ith_flashcard = array_of_flashcards[i];
+    let front_of_ith_flashcard = ith_flashcard.children[0].innerHTML;
+    let back_of_ith_flashcard = ith_flashcard.children[1].innerHTML;
+    current_deck[i][0] = front_of_ith_flashcard;
+    current_deck[i][1] = back_of_ith_flashcard; //updates values for current deck
+  }
   current_deck_of_cards = deck_of_cards.name_description_deck(name_box.value, description_box.value, current_deck);
   return true;
 }
 
 
 //toggles card viewing
+/*
 function view_cards(){
   if(current_deck.length === 0){
     alert("No cards to render!");
@@ -85,6 +96,7 @@ function view_cards(){
     deck_is_viewed = !deck_is_viewed;
   }
 }
+  */
 
 //renders cards
 function render_cards(){
@@ -122,9 +134,15 @@ function makeFlashcard(front, back){
 function edit_deck(){
   for(const flashcard of deck_viewer.children){
     for(const flashcard_side of flashcard.children){
-      flashcard_side.setAttribute("contenteditable", "true");
+      if(currently_editing){
+        flashcard_side.setAttribute("contenteditable", "false");
+      }
+      else{
+        flashcard_side.setAttribute("contenteditable", "true");
+      }
     }
   }
+  currently_editing = !currently_editing;
 }
 
 //saves the deck to localstorage
@@ -136,9 +154,13 @@ user must make a new session to make a new deck
 */
 function save_deck(){
   if(sessionStorage.getItem("already_saved") != null){ //we will check if they already saved a deck, which means we shouldn't push it again
-    makeDeck();
-    deck_of_decks[JSON.parse(sessionStorage.getItem("already_saved"))] = current_deck_of_cards;
-    localStorage.setItem("deck_of_decks", JSON.stringify(deck_of_decks));
+    if(makeDeck() === true){
+      deck_of_decks[JSON.parse(sessionStorage.getItem("already_saved"))] = current_deck_of_cards;
+      localStorage.setItem("deck_of_decks", JSON.stringify(deck_of_decks));
+    }
+    else{
+      alert("Make sure the name, description, and at least one card is filled out!");
+    }
     //the above should make it so that the deck of decks will replace our old (current) deck with the new stuff they put in / out
   }
   else{
@@ -147,6 +169,10 @@ function save_deck(){
       deck_of_decks.push(current_deck_of_cards);
       localStorage.setItem("deck_of_decks", JSON.stringify(deck_of_decks));
       sessionStorage.setItem("already_saved", JSON.stringify(deck_of_decks.length - 1));
+      /*
+      above code can be buggy if we are debugging and we clear our local storage but somehow
+      the session storage doesn't clear... so fix that
+      */
     }
     else{
       alert("Make sure the name, description, and at least one card is filled out!");
