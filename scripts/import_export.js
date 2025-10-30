4ccc9c23ecd15b0635caef6235fa3c6ee3385484
big-mike2 <migbike25@gmail.com>
2025-10-30 09:31:21 -0400

    added initial stuff for export select decks, added export empty decks

--- scripts/import_export.js
+++ scripts/import_export.js
@@ -2,7 +2,7 @@ var to_do_list = document.getElementById("download_to_do_list");
 var timer_stats = document.getElementById("download_timer");
 var deck_of_decks = document.getElementById("download_deck_of_decks");
 var deck_of_decks_empty = document.getElementById("download_deck_of_decks_empty");
-var select_decks = document.getElementById("download_select_decks");
+var select_decks = document.getElementById("export_select_decks");
 
 var export_buttons_holder = document.getElementById("export_buttons_holder");
 
@@ -12,13 +12,51 @@ var import_todo = document.getElementById("todolist");
 
 //from https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsText
 
-initialize_buttons();
+var dod_empty;
+var dod; //"deck of decks"
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("hello");
+    if(localStorage.getItem("deck_of_decks") === null){
+      console.log("set to empty");
+      dod = [];
+    }
+    else if(localStorage.getItem("deck_of_decks").indexOf("\"name_of_deck\":") === -1){
+      console.log("set to empty");
+      dod = [];
+    }
+      else{
+        console.log("set to nonempty");
+       dod = JSON.parse(localStorage.getItem("deck_of_decks"));
+    }
+    initialize_buttons();
+    initialize_select_deck();
+});
+
+function initialize_select_deck(){
+  
+}
+
+function make_empty_deck(target_dod){
+  let target_deck_of_decks = target_dod; //makes copy of it
+  for(var i = 0; i < target_deck_of_decks.length; i++){
+    let target_deck = target_deck_of_decks[i];
+    for(var j = 0; j < target_deck.deck.length; j++){
+      target_deck.deck[j][2] = '0';
+    }
+    target_deck_of_decks[i] = target_deck;
+  }
+  return target_deck_of_decks;
+}
+
 
 function initialize_buttons(){
   //will initialize to empty array if it doesn't work
   create_download_button(localStorage.getItem("deck_of_decks"), "all_decks_with_progress", "Download all your decks! (progress saved)");
   create_download_button(localStorage.getItem("study_stats"), "study_timer_stats", "Download your timer statistics!");
   create_download_button(localStorage.getItem("list_html"), "todolist", "Download your to-do list!");
+  create_download_button(JSON.stringify(make_empty_deck(dod)), "all_decks_without_progress", "Download all your decks! (progress deleted)");
 }
 
 //from https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
